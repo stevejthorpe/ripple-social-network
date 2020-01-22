e05d31462caf415091a40522d46682acf8aca446
stevejthorpe <thorpe.steve@gmail.com>
2020-01-22 16:41:03 +0100

    edit tables

--- sql/tables.sql
+++ sql/tables.sql
@@ -1,32 +1,31 @@
 DROP TABLE IF EXISTS users CASCADE;
-CREATE TABLE users(
-       id SERIAL PRIMARY KEY,
-       image_id SERIAL UNIQUE,
-       image VARCHAR(255),
-       firstname VARCHAR(255) NOT NULL CHECK (firstname != ''),
-       lastname VARCHAR(255) NOT NULL CHECK (lastname != ''),
-       email VARCHAR(255) NOT NULL UNIQUE CHECK (email != ''),
-       bio TEXT,
-       password VARCHAR(255) NOT NULL CHECK (password != ''),
-       created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    image character varying(255),
+    firstname character varying(255) NOT NULL,
+    lastname character varying(255) NOT NULL,
+    email character varying(255) NOT NULL UNIQUE,
+    bio text,
+    password character varying(255) NOT NULL,
+    created_at timestamp without time zone DEFAULT CURRENT_TIMESTAMP
 );
 
 DROP TABLE IF EXISTS friendships CASCADE;
-CREATE TABLE friendships(
+CREATE TABLE friendships (
     id SERIAL PRIMARY KEY,
-    receiver_id INT NOT NULL REFERENCES users(id),
-    sender_id INT NOT NULL REFERENCES users(id),
-    accepted BOOLEAN DEFAULT FALSE
+    receiver_id integer NOT NULL REFERENCES users(id),
+    sender_id integer NOT NULL REFERENCES users(id),
+    accepted boolean DEFAULT false
 );
 
 DROP TABLE IF EXISTS chat CASCADE;
-CREATE TABLE chat(
+CREATE TABLE chat (
     id SERIAL PRIMARY KEY,
-    receiver_id INT,
-    firstname VARCHAR(255) NOT NULL CHECK (firstname != ''),
-    lastname VARCHAR(255) NOT NULL CHECK (lastname != ''),
-    image VARCHAR(255),
-    sender_id INT NOT NULL,
-    msg TEXT,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    receiver_id integer,
+    sender_id integer NOT NULL,
+    msg text,
+    created_at timestamp without time zone DEFAULT CURRENT_TIMESTAMP,
+    firstname character varying(255),
+    lastname character varying(255),
+    image character varying(255)
 );
